f18b8bc48d6d6d691f5aab55de341e567e56f437
pravi-na <27pravina1997@gmail.com>
2022-02-28 13:38:33 +0530

    login details

--- src/component/loginpage.js
+++ src/component/loginpage.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 function Loginpage(){
+    const handleclick = (e) =>{
+        e.preventDefault();
+       var userinput =  document.querySelector(".userinput");
+       var userpassword = document.querySelector(".userpassword");
+        console.log("username:" +userinput.value);
+        console.log("userpassword:" +userpassword.value);
+    }
     return(
         <div>
             <div className="login">
@@ -8,7 +15,12 @@ function Loginpage(){
                 <img src={require("../assets/img.jpg")} alt="" className="loginimage" />
             </div>
             <div>
-                
+                <div>Login</div>
+                <form>
+                    <div><input type="text" placeholder="Enter your username" className="userinput" /></div>
+                    <div><input type="password" placeholder="Enter your password" className="userpassword" /></div>
+                    <div><button onClick={handleclick}>click</button></div>
+                </form>
             </div>
             </div>
         </div>
